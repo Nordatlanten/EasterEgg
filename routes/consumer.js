18e6966139f5726cadd4cd9a2d90acd6d38db61b
Atlan <gevaliatlaniskt@gmail.com>
2020-04-02 14:45:35 +0200

    created consumer.ejs and style.css (unfinished)

--- routes/consumer.js
+++ routes/consumer.js
@@ -3,8 +3,34 @@ const pool = require('../pool.js')
 
 const consumer = express.Router()
 
-consumer.route('/consumer').get((req, res) => {
-    res.send('hej')
+const { MongoClient } = require('mongodb')
+
+
+const pw = require('../pw.js')
+
+const uri = pw.mdbConnect
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+let db
+
+client.connect(err => {
+    if (err) console.log(err)
+    db = client.db('candydb').collection('producers')
 })
+
+
+
+consumer
+    .route('/consumer')
+    .get((req, res) => {
+        db.find().toArray((err, results) => {
+            if (err) console.log(err)
+           
+            res.render('./consumer.ejs', { p: results })
+        })
+    })
+
+
+
 
 module.exports = consumer
